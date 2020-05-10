d84dd43c27c344ef7d37f8a227d61f88a2dd4c52
Natiq Khan <natiqkhan00@gmail.com>
2020-05-10 17:33:29 +0200

    Version 1 done
    Todo: V2 with images

--- findeNemo/findeNemo.Designer.cs
+++ findeNemo/findeNemo.Designer.cs
@@ -30,48 +30,25 @@
         {
             this.components = new System.ComponentModel.Container();
             this.tmrGame = new System.Windows.Forms.Timer(this.components);
-            this.lblInfo = new System.Windows.Forms.Label();
-            this.pnlBackground = new System.Windows.Forms.Panel();
             this.lblPunkte = new System.Windows.Forms.Label();
             this.txbPunkte = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.btnRestart = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.lblMin = new System.Windows.Forms.Label();
-            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
             this.lblMax = new System.Windows.Forms.Label();
             this.pnlBlocks = new System.Windows.Forms.Panel();
-            this.pnlBackground.SuspendLayout();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.pnlSpeed = new System.Windows.Forms.Panel();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.pnlSpeed.SuspendLayout();
             this.SuspendLayout();
             // 
             // tmrGame
             // 
-            this.tmrGame.Enabled = true;
+            this.tmrGame.Interval = 50;
             this.tmrGame.Tick += new System.EventHandler(this.tmrGame_Tick);
-            // 
-            // lblInfo
-            // 
-            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInfo.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lblInfo.Location = new System.Drawing.Point(101, 11);
-            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(350, 88);
-            this.lblInfo.TabIndex = 0;
-            this.lblInfo.Text = "Klick auf Start\r\ndas ist die 1. Spielrunde";
-            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // pnlBackground
-            // 
-            this.pnlBackground.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.pnlBackground.Controls.Add(this.lblInfo);
-            this.pnlBackground.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pnlBackground.Location = new System.Drawing.Point(10, 11);
-            this.pnlBackground.Margin = new System.Windows.Forms.Padding(2);
-            this.pnlBackground.Name = "pnlBackground";
-            this.pnlBackground.Size = new System.Drawing.Size(573, 109);
-            this.pnlBackground.TabIndex = 0;
             // 
             // lblPunkte
             // 
@@ -86,11 +63,11 @@
             // 
             // txbPunkte
             // 
-            this.txbPunkte.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPunkte.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbPunkte.Location = new System.Drawing.Point(503, 197);
             this.txbPunkte.Margin = new System.Windows.Forms.Padding(2);
             this.txbPunkte.Name = "txbPunkte";
-            this.txbPunkte.Size = new System.Drawing.Size(72, 44);
+            this.txbPunkte.Size = new System.Drawing.Size(72, 29);
             this.txbPunkte.TabIndex = 3;
             this.txbPunkte.Text = "0";
             this.txbPunkte.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -104,6 +81,7 @@
             this.btnStart.TabIndex = 4;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnStop
             // 
@@ -114,6 +92,7 @@
             this.btnStop.TabIndex = 5;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // btnRestart
             // 
@@ -124,14 +103,7 @@
             this.btnRestart.TabIndex = 6;
             this.btnRestart.Text = "Neu starten";
             this.btnRestart.UseVisualStyleBackColor = true;
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(41, 667);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(419, 19);
-            this.progressBar1.TabIndex = 7;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
             // lblMin
             // 
@@ -143,15 +115,15 @@
             this.lblMin.TabIndex = 8;
             this.lblMin.Text = "Min";
             // 
-            // lblSpeed
+            // lblMsg
             // 
-            this.lblSpeed.AutoSize = true;
-            this.lblSpeed.Location = new System.Drawing.Point(202, 649);
-            this.lblSpeed.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblSpeed.Name = "lblSpeed";
-            this.lblSpeed.Size = new System.Drawing.Size(85, 13);
-            this.lblSpeed.TabIndex = 9;
-            this.lblSpeed.Text = "Geschwindigkeit";
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(202, 649);
+            this.lblMsg.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(85, 13);
+            this.lblMsg.TabIndex = 9;
+            this.lblMsg.Text = "Geschwindigkeit";
             // 
             // lblMax
             // 
@@ -171,27 +143,61 @@
             this.pnlBlocks.Size = new System.Drawing.Size(419, 509);
             this.pnlBlocks.TabIndex = 1;
             // 
+            // lblInfo
+            // 
+            this.lblInfo.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.AliceBlue;
+            this.lblInfo.Location = new System.Drawing.Point(11, 9);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(570, 102);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Klick auf Start\r\ndas ist die 1. Spielrunde";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlSpeed
+            // 
+            this.pnlSpeed.BackColor = System.Drawing.Color.PowderBlue;
+            this.pnlSpeed.Controls.Add(this.lblSpeed);
+            this.pnlSpeed.Location = new System.Drawing.Point(41, 665);
+            this.pnlSpeed.Name = "pnlSpeed";
+            this.pnlSpeed.Size = new System.Drawing.Size(419, 36);
+            this.pnlSpeed.TabIndex = 11;
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpeed.ForeColor = System.Drawing.Color.White;
+            this.lblSpeed.Location = new System.Drawing.Point(3, 5);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(10, 25);
+            this.lblSpeed.TabIndex = 0;
+            this.lblSpeed.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblSpeed.Visible = false;
+            // 
             // findeNemo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(592, 713);
+            this.Controls.Add(this.pnlSpeed);
+            this.Controls.Add(this.lblInfo);
             this.Controls.Add(this.lblMax);
-            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblMsg);
             this.Controls.Add(this.lblMin);
-            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnRestart);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txbPunkte);
             this.Controls.Add(this.lblPunkte);
             this.Controls.Add(this.pnlBlocks);
-            this.Controls.Add(this.pnlBackground);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "findeNemo";
             this.Text = "Finde Nemo";
             this.Load += new System.EventHandler(this.findeNemo_Load);
-            this.pnlBackground.ResumeLayout(false);
+            this.pnlSpeed.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,18 +205,18 @@
 
         #endregion
         private System.Windows.Forms.Timer tmrGame;
-        private System.Windows.Forms.Label lblInfo;
-        private System.Windows.Forms.Panel pnlBackground;
         private System.Windows.Forms.Label lblPunkte;
         private System.Windows.Forms.TextBox txbPunkte;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Button btnRestart;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label lblMin;
-        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblMsg;
         private System.Windows.Forms.Label lblMax;
         private System.Windows.Forms.Panel pnlBlocks;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Panel pnlSpeed;
+        private System.Windows.Forms.Label lblSpeed;
     }
 }
 
--- findeNemo/findeNemo.cs
+++ findeNemo/findeNemo.cs
@@ -1,55 +1,378 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace findeNemo
 {
     public partial class findeNemo : Form
     {
-        List<Block> tiles = new List<Block>();
-        int cols = 4;
-        int rows = 5;
-        Color blockGreen = Color.Green;
-        Color blockPink = Color.Pink;
-        Color blockRed = Color.Red;
+        /// <summary>
+        /// Counts every round of the game
+        /// </summary>
+        int round = 1;
+
+        /// <summary>
+        /// Counts score of the game
+        /// </summary>
+        int points = 0;
+
+        /// <summary>
+        /// Counter for button spacing
+        /// </summary>
+        int count = 0;
+
+        /// <summary>
+        /// button value counter
+        /// </summary>
+        int countBtns = 0;
+
+        /// <summary>
+        /// Game speed
+        /// </summary>
+        int gameSpeed = 0;
+
+        /// <summary>
+        /// Buttons movement
+        /// </summary>
+        int smoothness = 5;
+
+        /// <summary>
+        /// Genetrates a random number
+        /// </summary>
+        Random r = new Random();
+
+        /// <summary>
+        /// Generated button will be saved in here
+        /// </summary>
+        List <Button> buttons = new List<Button>();
 
         public findeNemo()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// This method loads the game with row of buttons and disables the following buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void findeNemo_Load(object sender, EventArgs e)
         {
-            int blockHeight = pnlBlocks.Height / rows;
-            int blockWidth = pnlBlocks.Width / cols;
-            var block = new Block(blockWidth, blockHeight, blockGreen);
+            //Generate 4 blocks on load
+            generateBlock();
 
+            //disable buttons
+            btnStop.Enabled = false;
+            btnRestart.Enabled = false;
+        }
 
-            for (int zeile = 0; zeile++ < rows;)
+        /// <summary>
+        /// This method start the game and disables the following buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Start timer
+            tmrGame.Start();
+
+            //disable or enable buttons
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnRestart.Enabled = true;
+        }
+
+        /// <summary>
+        /// This method stops the game and outputs a message in Info label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //Stop timer
+            tmrGame.Stop();
+            
+            //disable button
+            btnStop.Enabled = false;
+
+            //Disable all buttons in the panel
+            disableBtn();
+
+            //Echo message why?
+            echoMessage("Du hast das Spiel gestoppt - Die Spielrunde ist Vorüber\n Du hast " + points.ToString() + " Punkte und eine Geschwindigkeit von " + gameSpeed.ToString() + " erreicht");
+        }
+
+        /// <summary>
+        /// This method reset all value and add 1 to var round 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            //reset all settings to 0
+            points = 0;
+            gameSpeed = 0;
+            count = 0;
+            countBtns = 0;
+
+            //stop the timer
+            tmrGame.Stop();
+
+            //disable or enable buttons
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnRestart.Enabled = false;
+
+            //clear the panel
+            pnlBlocks.Controls.Clear();
+            buttons.Clear();
+            generateBlock();
+
+            //add 1 to rounds
+            round++;
+
+            //Echo message why?
+            echoMessage("Klicke auf Start\nDas ist die " + round.ToString() + ". Spielrunde");
+        }
+
+        /// <summary>
+        /// Game timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tmrGame_Tick(object sender, EventArgs e)
+        {
+            //Increment count
+            count++;
+            
+            //this line create space between buttons
+            if (count % smoothness == 0) generateBlock();
+            
+            //Buttons flowing downwards
+            goDown();
+        }
+
+        /// <summary>
+        /// This method checks if red button is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void redBtn_Click(object sender, EventArgs e)
+        {
+            //If the player clicks on red button then the game should start without clicking on start button
+            if (points == 0)
             {
-                for (int spalte = 0; spalte++ < cols;)
-                {
-                    int positionX = (spalte - 1) * blockWidth;
-                    int positionY = (zeile - 1) * blockHeight;
+                //timer start
+                tmrGame.Start();
 
-                    block.setPosition(positionX, positionY);
+                //disable or enables buttons
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                btnRestart.Enabled = true;
+            }
 
-                    //add blocks to panel
-                    block.drawOn(pnlBlocks);
-                    tiles.Add(block);
-                }
+            //Change button color to pink
+            (sender as Button).BackColor = Color.Pink;
+            
+            //Disbale the button from preventing points boosting
+            (sender as Button).Enabled = false;
+
+            //Take pionts from the button
+            points += Convert.ToInt32((sender as Button).Text);
+
+            //Echo the points in textbox
+            txbPunkte.Text = points.ToString();
+
+            //Increment the speed
+            controlSpeed();
+        }
+
+        /// <summary>
+        /// This method checks if green button is pressed
+        /// </summary>
+        /// <param name="sender">Object button</param>
+        /// <param name="e">Click event</param>
+        private void greenBtn_Click(object sender, EventArgs e)
+        {
+            //Timer stop
+            tmrGame.Stop();
+
+            //Disables all the buttons in the panel
+            disableBtn();
+
+            //Disable the following buttons
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+
+            //Echo message why?
+            echoMessage("Du hast das falsche Feld geklickt - Die Spielrunde ist vorüber.\nDu hast " + points.ToString() + " Punkte und eine Geschwindigkeit von " + gameSpeed.ToString() + " erreicht");
+        }
+
+        /// <summary>
+        /// This method will check if the button hits panel bottom, then game will stop
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void checkBtn(object sender, EventArgs e)
+        {
+            //Checking if Red button touched the bottom of the panel
+            if (Color.Red == (sender as Button).BackColor && (sender as Button).Top > pnlBlocks.Height)
+            {
+                //if yes stop the timer
+                tmrGame.Stop();
+
+                //Disable the buttons from preventing to click and points adding
+                disableBtn();
+                
+                //Disable or enables the buttons
+                btnStop.Enabled = false;
+                btnRestart.Enabled = true;
+
+                //Echo message why?
+                echoMessage("Du hast in der untersten Reihe nicht geklickt - Die Spielrunde ist vorüber.\nDu hast " + points.ToString() + " Punkte und eine Geschwindigkeit von " + gameSpeed.ToString() + " erreicht");
             }
         }
 
-        private void tmrGame_Tick(object sender, EventArgs e)
+
+        /// <summary>
+        /// This method generates row of buttons in panel
+        /// </summary>
+        private void generateBlock()
         {
-            
+            //Generate the random button
+            int randomBtn = r.Next(0, 4);
+
+            //Generate button when the form is loaded
+            if (countBtns == 0)
+            {
+                //Generate 4 buttons each row 
+                for (int i = 0; i < 4; i++)
+                {
+                    //Increment countbtns 
+                    countBtns++;
+
+                    //Create random red button
+                    bool redBtn = (i == randomBtn);
+
+                    //Add button to the panel
+                    Button btn = addBtn(i, redBtn, 0);
+                    this.pnlBlocks.Controls.Add(btn);
+                    buttons.Add(btn);
+                }
+            }
+
+            //click on start or press the red button
+            for (int i = 0; i < 4; i++)
+            {
+                //Increment countbtns 
+                countBtns++;
+
+                //Create random red button
+                bool redBtn = (i == randomBtn);
+
+                //Add button to the panel
+                Button btn = addBtn(i, redBtn, -50);
+                this.pnlBlocks.Controls.Add(btn);
+                buttons.Add(btn);
+            }
+        }   
+
+        /// <summary>
+        /// This method generates a button
+        /// </summary>
+        /// <param name="i">This param is for button name</param>
+        /// <param name="redBtn">This param is for the red button</param>
+        /// <param name="btnY">This param defeines the Location Y of button</param>
+        /// <returns>And returns the generated button</returns>
+        private Button addBtn(int i, bool redBtn, int btnY)
+        {
+            //Button object
+            Button btn = new Button();
+
+            //Button settings
+            btn.Name = "btn" + i.ToString();
+            btn.Text = countBtns.ToString();
+            btn.Width = pnlBlocks.Width / 4;
+            btn.Height = 50;
+            btn.Location = new Point(i * (pnlBlocks.Width / 4), btnY);
+            btn.BackColor = redBtn ? Color.Red : Color.CadetBlue;            
+            btn.Click += redBtn ? new EventHandler(this.redBtn_Click) : new EventHandler(this.greenBtn_Click);
+            btn.LocationChanged += new EventHandler(this.checkBtn);
+
+            //Returns the button
+            return btn;
+        }
+        
+        /// <summary>
+        /// This method moves the button downwards by adding + 5 px
+        /// </summary>
+        private void goDown()
+        {
+            //Loop through all buttons
+            foreach (var btn in buttons)
+            {
+                //Change Y location each button
+                btn.Top = btn.Location.Y + 50 / smoothness;
+            }
+        }
+
+        /// <summary>
+        /// This methods disables buttons in the list
+        /// </summary>
+        private void disableBtn()
+        {
+            //Loop through all buttons
+            foreach (var btn in buttons)
+            {
+                //Disable the button
+                btn.Enabled = false;
+            }
+        }
+        
+        /// <summary>
+        /// This method shows the given message in Info label
+        /// </summary>
+        /// <param name="message">Given message</param>
+        private void echoMessage(string message)
+        {
+            //Show message in Label Info
+            lblInfo.Text = message;
+        }
+        
+        /// <summary>
+        /// This method controls & shows game speed in progress bar
+        /// </summary>
+        private void controlSpeed()
+        {
+            //Increment the speed
+            gameSpeed++;
+
+            //If speed is 100 or more and Timer tick value = 20, this is the max speed
+            if (gameSpeed >= 100)
+            {
+                tmrGame.Interval = 20;
+            }
+            else 
+            {
+                tmrGame.Interval = 120 - gameSpeed;
+            }
+
+            //Hide speed label, if speed is 0
+            if (gameSpeed == 0)
+            {
+                lblSpeed.Visible = false;
+            }
+            else
+            {
+                //show the label
+                lblSpeed.Visible = true;
+
+                //show the speed in panel speed
+                lblSpeed.Width = pnlSpeed.Width / 100 * gameSpeed;
+                //with speed counter
+                lblSpeed.Text = gameSpeed.ToString();
+            }
         }
     }
 }
